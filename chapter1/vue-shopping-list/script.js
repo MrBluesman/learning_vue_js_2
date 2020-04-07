6070cffc4bef1c753dc165a447ae3d8ecaa653ad
Lukasz Parysek <l.parysek@gmail.com>
2020-04-07 20:38:52 +0200

    Implement displaying list with toggling completion status of item

--- chapter1/vue-shopping-list/script.js
+++ chapter1/vue-shopping-list/script.js
@@ -1,5 +1,19 @@
 const data = {
   title: 'Shopping List',
+  items: [
+    {
+      name: 'Babana',
+      completed: false,
+    },
+    {
+      name: 'Tomato',
+      completed: false,
+    },
+    {
+      name: 'Garlic',
+      completed: true
+    }
+  ]
 };
 
 new Vue({
