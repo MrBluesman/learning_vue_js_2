248a392f58194ff45882d5182db7809e6132dee7
Lukasz Parysek <l.parysek@gmail.com>
2020-04-14 20:45:35 +0200

    Add displaying random cats while resting

--- chapter1/vue-pomodoro/script.js
+++ chapter1/vue-pomodoro/script.js
@@ -7,8 +7,8 @@ const STATES = {
   STOPPED: 'stopped',
   PAUSED: 'paused',
 };
-const WORKING_TIME_LENGTH_IN_MINUTES = 25;
-const RESTING_TIME_LENGTH_IN_MINUTES = 5;
+const WORKING_TIME_LENGTH_IN_MINUTES = 1;
+const RESTING_TIME_LENGTH_IN_MINUTES = 1;
 
 new Vue({
   el: '#app',
@@ -17,8 +17,9 @@ new Vue({
     minute: WORKING_TIME_LENGTH_IN_MINUTES,
     second: 0,
     pomodoroState: POMODORO_STATES.WORK,
-    timestamp: 0,
-    interval: null
+    tickInterval: null,
+    imageInterval: null,
+    imageUrl: null,
   },
   computed: {
     title: function () {
@@ -29,21 +30,36 @@ new Vue({
     },
     sec: function () {
       return this.leftPad(this.second);
-    }
+    },
+    isRestingTime: function () {
+      return this.pomodoroState === POMODORO_STATES.REST;
+    },
   },
   methods: {
     start: function () {
       this.state = STATES.STARTED;
       this._tick();
-      this.interval = setInterval(this._tick, 1000);
+      this.tickInterval = setInterval(this._tick, 1000);
+      this.getCatImage();
+
+      // If state has been switched to resting time,
+      // start interval for display cats
+      this.setCatImageInterval();
     },
     pause: function () {
       this.state = STATES.PAUSED;
-      clearInterval(this.interval);
+
+      clearInterval(this.tickInterval);
+      clearInterval(this.imageInterval);
+      this.imageInterval = null;
     },
     stop: function () {
       this.state = STATES.STOPPED;
-      clearInterval(this.interval);
+
+      clearInterval(this.tickInterval);
+      clearInterval(this.imageInterval);
+      this.imageInterval = null;
+
       this.pomodoroState = POMODORO_STATES.WORK;
       this.minute = WORKING_TIME_LENGTH_IN_MINUTES;
       this.second = 0;
@@ -72,9 +88,36 @@ new Vue({
       this.minute = this.pomodoroState === POMODORO_STATES.WORK
         ? WORKING_TIME_LENGTH_IN_MINUTES
         : RESTING_TIME_LENGTH_IN_MINUTES;
+
+      // If state has been switched to resting time,
+      // start interval for display cats
+      this.setCatImageInterval();
     },
-    leftPad(value) {
+    leftPad: function (value) {
       return value < 10 ? '0' + value : value;
+    },
+    setCatImageInterval: function () {
+      if (this.isRestingTime && !this.imageInterval) {
+        this.imageInterval = setInterval(this.getCatImage, 5000);
+      } else {
+        clearInterval(this.imageInterval);
+        this.imageInterval = null;
+      }
+    },
+    getCatImage: function () {
+      const request = new XMLHttpRequest();
+
+      request.open('GET', 'https://api.thecatapi.com/v1/images/search', true);
+      request.setRequestHeader('x-api-key', '88fa2ba1-4cd3-4381-a238-1f58016a0144');
+      request.send();
+
+      request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+          if (request.status === 200) {
+            this.imageUrl = JSON.parse(request.response)[0].url;
+          }
+        }
+      };
     }
   }
 });
