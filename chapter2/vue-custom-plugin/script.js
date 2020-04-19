baf86e83fae7eda868ffce53aaf5b72d84e1ad84
Lukasz Parysek <l.parysek@gmail.com>
2020-04-19 10:28:07 +0200

    Instantiate vue with use created custom math plugin

---
+++ chapter2/vue-custom-plugin/script.js
@@ -0,0 +1,13 @@
+import Vue from 'vue/dist/vue,js';
+import VueMathPlugin from './VueMathPlugin';
+
+Vue.use(VueMathPlugin);
+
+new Vue({
+  el: '#app',
+  data: function () {
+    return {
+      item: 49
+    };
+  }
+});
