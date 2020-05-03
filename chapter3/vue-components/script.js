2684abd8acd44e89aad64fd6ae08f2c087a8f69d
Lukasz Parysek <l.parysek@gmail.com>
2020-05-03 21:12:30 +0200

    Components inside other components

--- chapter3/vue-components/script.js
+++ chapter3/vue-components/script.js
@@ -1,3 +1,15 @@
+// register form component
+Vue.component('form-component', {
+  template: '#form',
+  props: ['value'],
+  methods: {
+    onInput: function (event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+});
+
+// register hello component
 // 1. both initialization and registration can be written as single
 // Vue.component invocation with corresponding options
 // 2. calling parent's data attributes in the component
@@ -11,10 +23,17 @@ Vue.component('hello-component', {
   props: ['user']
 });
 
+// create greetings component base on the greetings template
+Vue.component('greetings-component', {
+  template: '#greetings',
+  data: function () {
+    return {
+      user: 'hero'
+    };
+  }
+});
+
 // initializing the Vue application
 new Vue({
   el: '#app',
-  data: {
-    user: 'hero'
-  }
 });
