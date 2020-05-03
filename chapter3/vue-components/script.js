e23841678578af8bd6f814551e2404495b7f95ba
Lukasz Parysek <l.parysek@gmail.com>
2020-05-03 20:34:15 +0200

    Scope of the components

--- chapter3/vue-components/script.js
+++ chapter3/vue-components/script.js
@@ -1,13 +1,20 @@
-// both initialization and registration can be written as single
+// 1. both initialization and registration can be written as single
 // Vue.component invocation with corresponding options
+// 2. calling parent's data attributes in the component
 Vue.component('hello-component', {
-  template: '<h1>Hello</h1>',
+  template: '#hello',
   data: function () {
-    return { msg: 'Hello' };
-  }
+    return {
+      msg: 'Hello'
+    };
+  },
+  props: ['user']
 });
 
 // initializing the Vue application
 new Vue({
-  el: '#app'
+  el: '#app',
+  data: {
+    user: 'hero'
+  }
 });
