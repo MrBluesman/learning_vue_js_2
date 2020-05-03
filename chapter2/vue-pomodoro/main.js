4135075dd2296527cf6ab064f251dbb8d8ed776e
Lukasz Parysek <l.parysek@gmail.com>
2020-05-03 13:14:10 +0200

    Fulfill manifest, add awesome icons, amendments

--- chapter2/vue-pomodoro/main.js
+++ chapter2/vue-pomodoro/main.js
@@ -1,10 +1,10 @@
-chrome.app.runtime.onLaunched.addListener(() => {
+chrome.app.runtime.onLaunched.addListener(function () {
   const screenWidth = screen.availWidth;
   const screenHeight = screen.availHeight;
   const width = 500;
   const height = 300;
 
-  chrome.app.window.create('../index.html', {
+  chrome.app.window.create('index.html', {
     id: 'pomodoroVueID',
     outerBounds: {
       width,
