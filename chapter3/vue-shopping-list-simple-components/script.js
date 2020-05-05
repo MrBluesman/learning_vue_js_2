d75c41c14ee468177aaed96674c14d62f7c64e55
Lukasz Parysek <l.parysek@gmail.com>
2020-05-05 22:37:19 +0200

    Defining and registering all the components

--- chapter3/vue-shopping-list-simple-components/script.js
+++ chapter3/vue-shopping-list-simple-components/script.js
@@ -14,24 +14,60 @@ const data = {
       completed: true
     }
   ],
-  newItem: '',
 };
+
+// add item component
+Vue.component('add-item-component', {
+  template: '#add-item-template',
+  data: function () {
+    return {
+      newItem: ''
+    };
+  },
+  methods: {
+    addItem: function () {
+      console.log('Implement adding');
+    }
+  }
+});
+
+// item component
+Vue.component('item-component', {
+  template: '#item-template',
+  props: ['item']
+});
+
+// items component
+Vue.component('items-component', {
+  template: '#items-template',
+  props: ['items']
+});
+
+Vue.component('change-title-component', {
+  template: '#change-title-template',
+  props: ['value'],
+  methods: {
+    onInput: function (event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+});
 
 new Vue({
   el: '#shopping-list',
   data,
   methods: {
     addItem() {
-      if (!this.newItem || this.items.filter(i => i.name === this.newItem).length > 0) {
-        return;
-      }
-
-      this.items.push({
-        name: this.newItem,
-        completed: false
-      });
-
-      this.newItem = '';
+      // if (!this.newItem || this.items.filter(i => i.name === this.newItem).length > 0) {
+      //   return;
+      // }
+      //
+      // this.items.push({
+      //   name: this.newItem,
+      //   completed: false
+      // });
+      //
+      // this.newItem = '';
     }
   }
 });
