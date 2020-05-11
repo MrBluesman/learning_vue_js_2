0b898aa24bcb06d600f3bc276e6cf6aee4e3fed9
Lukasz Parysek <l.parysek@gmail.com>
2020-05-11 20:41:24 +0200

    Invoke all subcomponents in main app component

--- chapter3/pomodoro/src/App.vue
+++ chapter3/pomodoro/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <div id="app">
-    Pomodoro
+  <div id="app"
+       class="container">
+    <h2>
+      <span>Pomodoro</span>
+      <ControlsComponent/>
+    </h2>
+    <StateTitleComponent/>
+    <CountdownComponent/>
+    <KittensComponent/>
   </div>
 </template>
 
 <script>
+import ControlsComponent from './components/ControlsComponent';
+import StateTitleComponent from './components/StateTitleComponent';
+import CountdownComponent from './components/CountdownComponent';
+import KittensComponent from './components/KittensComponent';
 
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    KittensComponent,
+    CountdownComponent,
+    StateTitleComponent,
+    ControlsComponent
+  }
 };
 </script>
 
