b5d505b800f1ead4df889bf3ae0655ac1fb82abe
Lukasz Parysek <l.parysek@gmail.com>
2020-05-24 11:53:00 +0200

    Copy pomodoro base to study expressions and filters

---
+++ chapter4/pomodoro2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app"
+       class="container">
+    <h2>
+      <span>Pomodoro</span>
+      <ControlsComponent/>
+    </h2>
+    <StateTitleComponent/>
+    <CountdownComponent class="mb-2"/>
+    <KittensComponent/>
+  </div>
+</template>
+
+<script>
+import ControlsComponent from './components/ControlsComponent';
+import StateTitleComponent from './components/StateTitleComponent';
+import CountdownComponent from './components/CountdownComponent';
+import KittensComponent from './components/KittensComponent';
+
+export default {
+  name: 'App',
+  components: {
+    KittensComponent,
+    CountdownComponent,
+    StateTitleComponent,
+    ControlsComponent
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ chapter4/pomodoro2/src/components/ControlsComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="btn-group">
+    <button class="btn btn-outline-secondary mr-1 ml-2">
+      <font-awesome-icon icon="play"/>
+    </button>
+    <button class="btn btn-outline-secondary mr-1">
+      <font-awesome-icon icon="pause"/>
+    </button>
+    <button class="btn btn-outline-secondary">
+      <font-awesome-icon icon="stop"/>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ControlsComponent'
+};
+</script>
+
+<style scoped>
+
+</style>
