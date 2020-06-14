e6f6a9ed48a2f8951759e942a87140d33251820b
Lukasz Parysek <l.parysek@gmail.com>
2020-06-14 21:48:03 +0200

    Copy shopping list project to chapter 4 learning purposes

---
+++ chapter4/shopping-list/src/components/AddItemComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="add-item">
+    <div class="input-group">
+      <input type="text"
+             @keyup.enter="addItem"
+             v-model="newItem"
+             class="form-control"
+             placeholder="Add shopping list item">
+      <span class="input-group-btn">
+        <button class="btn btn-default"
+                @click="addItem"
+                type="button">Add!</button>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddItemComponent',
+  data() {
+    return {
+      newItem: '',
+    };
+  },
+  methods: {
+    addItem() {
+      this.$emit('on-add-item', {
+        name: this.newItem,
+        onSuccessFullAdd: () => {
+          this.newItem = '';
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
