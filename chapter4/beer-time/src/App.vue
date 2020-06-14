e8437b8d7a582844b5b805783931d9ddfde26b7d
Lukasz Parysek <l.parysek@gmail.com>
2020-06-14 21:07:22 +0200

    Implement example for conditional rendering

--- chapter4/beer-time/src/App.vue
+++ chapter4/beer-time/src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="app">
-
+    <h1 v-if="isAdmin">Beer Time!</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+  data() {
+    return {
+      isAdmin: false,
+    };
+  }
 };
 </script>
