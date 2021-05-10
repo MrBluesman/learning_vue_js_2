7ad83da6676f3277ef4c55b912e9cc7a4170157a
Lukasz Parysek <l.parysek@gmail.com>
2021-05-10 19:55:12 +0200

    Copy pomodoro example app to chapter 5

---
+++ chapter5/pomodoro/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app"
+       class="container">
+    <h2>
+      <span>{{ 'Pomodoro' | uppercase }}</span>
+      <ControlsComponent/>
+    </h2>
+    <StateTitleComponent :is-working="isWorking"/>
+    <CountdownComponent class="mb-2"/>
+    <transition name="fade">
+      <KittensComponent v-show="!isWorking"/>
+    </transition>
+  </div>
+</template>
+
+<script>
+import ControlsComponent from './components/ControlsComponent';
+import StateTitleComponent from './components/StateTitleComponent';
+import CountdownComponent from './components/CountdownComponent';
+import KittensComponent from './components/KittensComponent';
+
+window.data = {
+  kittens: true,
+  isWorking: true
+};
+
+export default {
+  name: 'App',
+  components: {
+    KittensComponent,
+    CountdownComponent,
+    StateTitleComponent,
+    ControlsComponent
+  },
+  data() {
+    return window.data;
+  }
+};
+</script>
+
+<style lang="scss">
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
