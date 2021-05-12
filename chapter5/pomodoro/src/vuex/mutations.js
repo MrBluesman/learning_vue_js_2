29ea79c7879a8f2e204606dc1118e70e3ab7ccbf
Lukasz Parysek <l.parysek@gmail.com>
2021-05-12 18:38:04 +0200

    Creating the Pomodoro timer [pomodoro]

--- chapter5/pomodoro/src/vuex/mutations.js
+++ chapter5/pomodoro/src/vuex/mutations.js
@@ -1,19 +1,38 @@
 import { START, PAUSE, STOP } from '@/vuex/mutation_types';
+import { RESTING_TIME, WORKING_TIME } from '@/config';
+
+function togglePomodoro(state, toggle = false) {
+  toggle = toggle || !state.isWorking;
+  state.isWorking = toggle;
+  state.counter = state.isWorking ? WORKING_TIME : RESTING_TIME;
+}
+
+function tick(state) {
+  if (state.counter === 0) {
+    togglePomodoro(state);
+  }
+
+  state.counter--;
+}
 
 export default {
   [START](state) {
     state.started = true;
     state.paused = false;
     state.stopped = false;
+    state.interval = setInterval(() => tick(state), 1000);
   },
   [PAUSE](state) {
     state.started = true;
     state.paused = true;
     state.stopped = false;
+    clearInterval(state.interval);
   },
   [STOP](state) {
     state.started = false;
     state.paused = false;
     state.stopped = true;
+    clearInterval(state.interval);
+    togglePomodoro(state, true);
   }
 }
