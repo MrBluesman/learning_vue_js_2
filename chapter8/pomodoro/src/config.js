ad0dbcf993d1da0cc96e71a3acf5d3cec44eddb6
Łukasz Parysek <l.parysek@icloud.com>
2023-08-25 12:28:10 +0200

    Run e2e tests in CI [pomodoro] [chapter 8]

--- chapter8/pomodoro/src/config.js
+++ chapter8/pomodoro/src/config.js
@@ -1,5 +1,5 @@
 // Change to 0.1 * 60 for e2e tests
-// export const WORKING_TIME = 0.1 * 60;
-export const WORKING_TIME = 20 * 60;
+export const WORKING_TIME = 0.1 * 60;
+// export const WORKING_TIME = 20 * 60;
 export const RESTING_TIME = 5 * 60;
 export const KITTEN_TIME = 5; //each kitten is visible for 5 seconds
